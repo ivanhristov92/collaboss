66120f021972e164d2a46554e6aedb05b8a07a8a
qfintern <angelina@quikfox.com>
2017-08-30 17:04:52 +0300

    Initial commit

---
+++ BST.js
@@ -0,0 +1,32 @@
+
+
+var node = {
+    value: 125,
+    left: null,
+    right: null
+};
+
+
+class BST {
+
+    constructor(){
+        this._root = null;
+    }
+
+    add(value){}
+
+    contains(value){}
+
+    remove(value){}
+
+    traverse(process){}
+
+    size(){}
+
+    toArray(){}
+
+    toString(){}
+
+}
+
+
